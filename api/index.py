9dc2c45455d4fe0554c77f9933d8623fe886521e
TALANTPMS <Savelprz2008@gmail.com>
2025-05-17 18:31:57 +0500

    Update project structure for Vercel deployment

--- api/index.py
+++ api/index.py
@@ -3,6 +3,6 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from action import app
 
-# For Vercel Serverless Function
-def handler(request, context):
-    return app(request)
+# For Vercel serverless function
+def handler(request):
+    return app
